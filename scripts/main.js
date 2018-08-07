5783b8756551bb9ede8a7fe71658414d107cba50
Melchi Mashiku <Mashiku@Melchis-MacBook-Pro.local>
2018-08-07 09:48:22 -0400

    Posts not complete yet

--- scripts/main.js
+++ scripts/main.js
@@ -61,52 +61,36 @@ function writeNewPost(uid, username, picture, title, note, work_date, work_activ
 /**
  * Creates a post element.
  */
-function createPostElement(postId, title, text, author, authorId, authorPic) {
+function createPostElement(postId, title, text, author, authorId, authorPic,time) {
   var uid = firebase.auth().currentUser.uid;
 
-  var html =
-      '<div class="post post-' + postId + ' mdl-cell mdl-cell--12-col ' +
-                  'mdl-cell--6-col-tablet mdl-cell--4-col-desktop mdl-grid mdl-grid--no-spacing">' +
-        '<div class="mdl-card mdl-shadow--2dp">' +
-          '<div class="mdl-card__title mdl-color--black mdl-color-text--white">' +
-            '<h4 class="mdl-card__title-text"></h4>' +
-          '</div>' +
-          '<div class="header">' +
-            '<div>' +
-              '<div class="avatar"></div>' +
-              '<div class="username mdl-color-text--black"></div>' +
-            '</div>' +
-          '</div>' +
-          '<span class="star">' +
-            '<div class="not-starred material-icons">star_border</div>' +
-            '<div class="starred material-icons">star</div>' +
-            '<div class="star-count">0</div>' +
-          '</span>' +
-          '<div class="text"></div>' +
-          '<div class="comments-container"></div>' +
-          '<form class="add-comment" action="#">' +
-            '<div class="mdl-textfield mdl-js-textfield">' +
-              '<input class="mdl-textfield__input new-comment" type="text">' +
-              '<label class="mdl-textfield__label">Comment...</label>' +
-            '</div>' +
-          '</form>' +
-        '</div>' +
-      '</div>';
+  var html = '<div class="post post-' + postId + 'mdl-layout mdl-js-layout">'+
+  '		<div class="transbg mdl-card mdl-shadow--6dp">'+
+  '				<div class="innerdiv mdl-card mdl-shadow--6dp">'+
+  '					<div class="mdl-card__title_post">'+
+  '				<h5 class="mdl-card__title-text__name_post">'+title+'</h5>'+
+  '						<h5 class="mdl-card__title-text__activity_post">'+text+'</h5>'+
+  '			</div>'+
+  '					<div class="mdl-card__time"><h6 class="mdl-card__title-text_post mdl-shadow--6dp">'+ time +'</h6></div>'+
+  '				</div>'+
+  '		</div>'+
+  '</div>';
+      
 
   // Create the DOM element from the HTML.
   var div = document.createElement('div');
   div.innerHTML = html;
   var postElement = div.firstChild;
-  if (componentHandler) {
-    componentHandler.upgradeElements(postElement.getElementsByClassName('mdl-textfield')[0]);
-  }
+  // if (componentHandler) {
+  //   componentHandler.upgradeElements(postElement.getElementsByClassName('mdl-textfield')[0]);
+  // }
 
   // Set values.
-  postElement.getElementsByClassName('text')[0].innerText = text;
-  postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = title;
-  postElement.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
-  postElement.getElementsByClassName('avatar')[0].style.backgroundImage = 'url("' +
-      (authorPic || './silhouette.jpg') + '")';
+  // postElement.getElementsByClassName('text')[0].innerText = text;
+  // postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = title;
+  // postElement.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
+  // postElement.getElementsByClassName('avatar')[0].style.backgroundImage = 'url("' +
+  //     (authorPic || './silhouette.jpg') + '")';
 
 
   // Keep track of all Firebase reference on which we are listening.
@@ -142,7 +126,7 @@ function startDatabaseQueries() {
       var author = data.val().author || 'Anonymous';
       var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
       containerElement.insertBefore(
-        createPostElement(data.key, data.val().title, data.val().note, author, data.val().uid, data.val().authorPic),
+        createPostElement(data.key, data.val().title, data.val().note, author, data.val().uid, data.val().authorPic, data.val().deepworkquantity),
         containerElement.firstChild);
     });
     postsRef.on('child_changed', function(data) {
